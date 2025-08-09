8c40a43816a3987f7063ab16ffecd41d4efcfc79
nick-p-34 <117747730+nick-p-34@users.noreply.github.com>
2025-08-09 21:05:56 +0100

    Initial Commit

---
+++ src/main/java/com/example/telemetrix/TelemetrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.telemetrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/telemetrix/TelemetrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telemetrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
